149cada86355a39c3e7359e83162649bd3a0b3c6
Adam Morris <zwack.am@gmail.com>
2017-04-28 17:00:13 -0700

    Added an Install script for Ansible

---
+++ ansible_install.sh
@@ -0,0 +1,4 @@
+echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main > /etc/apt/sources.list.d/ansible.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get update
+apt-get install ansible
